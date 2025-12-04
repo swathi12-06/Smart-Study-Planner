40da767c42fcf84b23e61549c7a7c15e20eaa939
swathi12-06 <swathiinduri10@gmail.com>
2025-12-04 21:41:21 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open("focusflow-cache").then(cache => {
+    return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+  }));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
