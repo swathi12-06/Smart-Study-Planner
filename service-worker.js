4ed5a3a0595f48c223568be05b044fafaad1f5f6
swathi12-06 <swathiinduri10@gmail.com>
2025-12-07 15:28:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,20 @@
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("focusflow-cache").then(cache => {
-    return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+  e.waitUntil(caches.open("focusflow-cache-v2").then(cache => {
+    return cache.addAll(["/", "/index.html", "/style.css", "/script.js", "/manifest.json", "/favicon.ico"]);
   }));
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(clients.matchAll({ type: "window" }).then(clientList => {
+    if (clientList.length > 0) {
+      const client = clientList[0];
+      return client.focus();
+    }
+    return clients.openWindow('/');
+  }));
 });
